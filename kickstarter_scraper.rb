e55db6164426dc14360505af0dafc0f9ca6faf3a
Jay Bednar <ironviralmedia@gmail.com>
2018-02-21 19:17:19 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,7 +6,7 @@ require 'nokogiri'
 
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
-
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
 
 html = File.read('fixtures/kickstarter.html')
 kickstarter = Nokogiri::HTML(html)
