d44233dcde60d31bebcd1bf855b833cbbe8214ca
Jay Bednar <ironviralmedia@gmail.com>
2018-02-21 19:25:01 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,7 +9,7 @@ require 'nokogiri'
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 # location: project.css(".project-meta li a .location-name").text
-
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
 
 html = File.read('fixtures/kickstarter.html')
